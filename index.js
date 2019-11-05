fb70b8082fb093f996f087e551fe62d520e8e898
Amy Kapernick <amy@aimhigherweb.design>
2019-11-06 05:07:10 +0800

    running express to check if there is a quokka

--- index.js
+++ index.js
@@ -4,21 +4,59 @@ const express = require("express"),
   nunjucks = require("nunjucks"),
   util = require("util"),
   fs = require("fs"),
-  PredictionApiClient = require("@azure/cognitiveservices-customvision-prediction");
+  TrainingApi = require("@azure/cognitiveservices-customvision-training"),
+  PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
 
 const app = express(),
-  setTimeoutPromise = util.promisify(setTimeout),
-  predictionKey = process.env.PREDICTION_KEY;
+  key = process.env.API_KEY,
+  endpoint = "https://southcentralus.api.cognitive.microsoft.com/",
+  publishIterationName = process.env.ITERATION,
+  projectId = process.env.PROJECT_ID;
 
-app.use(express.static("/public"));
+const predictor = new PredictionApi.PredictionAPIClient(key, endpoint),
+  testFile = `quokka_(0).jpg`;
+
+const customVision = async () => {
+  return (results = await predictor.classifyImage(
+    projectId,
+    publishIterationName,
+    fs.readFileSync(`./img/${testFile}`)
+  ));
+};
+
+app.use(express.static("img"));
 
 const env = nunjucks.configure(["views/"], {
   autoescape: true,
   express: app
 });
 
-app.get("/", (req, res) => {
-  res.render("index.html", { title: "Quokka or Not" });
+app.get("/", async (req, res) => {
+  const results = await customVision();
+
+  let outcome = [];
+
+  results.predictions.forEach(tag => {
+    if (tag.tagName == "Negative") {
+      outcome[0] = tag.probability;
+    } else if (tag.tagName == "Quokka") {
+      outcome[1] = tag.probability;
+    }
+  });
+
+  if (outcome[0] > outcome[1]) {
+    outcome = false;
+  } else {
+    outcome = true;
+  }
+
+  console.log(results.predictions);
+  res.render("index.html", {
+    title: "Quokka or Not",
+    results: results,
+    image: testFile,
+    outcome: outcome
+  });
 });
 
 app.listen(3000, () => {
