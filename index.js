e7acb927245b2ddbce956228f564c192b45d3fbd
Amy Kapernick <amy@aimhigherweb.design>
2019-11-06 22:30:37 +0800

    changing launch

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Quokka or Not listening on port 3000");
 });
