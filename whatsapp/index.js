3e48f99cc32c0bdfe00a68ed11f0a4aef605bd7f
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 17:31:18 +0800

    sending image url in message

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -6,10 +6,8 @@ module.exports = async function (context, req) {
     twiml = new MessagingResponse(),
     message = twiml.message(),
     image = req.body.MediaUrl0
-
-    console.log(image)
     
-    message.body(`Welcome to Quokkabot!`)
+    message.body(`Welcome to Quokkabot!\n ${image}`)
 
     res.set('content-type', 'text/xml')
 	res.end(message.toString())
