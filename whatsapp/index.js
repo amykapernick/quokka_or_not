178ff6ff6ea39ab2d9ec2a1c458f4fe8b78dc78e
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 17:33:49 +0800

    finding req object

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 
-module.exports = async function (context, req) {
+module.exports = async function (context) {
     const res = context.res,
     MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse(),
     message = twiml.message(),
-    image = req.body.MediaUrl0
+    image = context.req.body.MediaUrl0
+
+    console.log(image)
+    console.log(context.req.body)
     
-    message.body(`Welcome to Quokkabot!\n ${image}`)
+    message.body(
+        `Welcome to Quokkabot!\n
+        ${image}`
+    )
 
     res.set('content-type', 'text/xml')
 	res.end(message.toString())
