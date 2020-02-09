5f0128a57d4daaff05ce0c8c9b2d2600fbd683fa
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 14:21:55 +0800

    testing function

--- quokka_or_not/index.js
+++ quokka_or_not/index.js
@@ -1,16 +1,19 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, res) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    const bodyParser = require('body-parser'),
+	accountSid = process.env.ACCOUNT_SID,
+	authToken = process.env.AUTH_TOKEN,
+    client = require('twilio')(accountSid, authToken),
+    MessagingResponse = client.twiml.MessagingResponse,
+    twiml = new MessagingResponse()
+    message = twiml.message()
+    
+    message.body(`This is a quokka`)
+    message.media(`https://quokkas.amyskapers.dev/img/quokka_(1).jpg`)
+
+    res.send('Welcome to quokkabot')
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' })
+	res.end(message.toString())
 };
