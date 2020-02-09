72b41c589662d5c841371e51316cfea455f8622e
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 15:06:30 +0800

    cleaned up function to bare minimum to work with whatsapp

--- quokka_or_not/index.js
+++ quokka_or_not/index.js
@@ -1,22 +1,13 @@
 require('dotenv').config()
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const res = context.res, bodyParser = require('body-parser'),
-	accountSid = process.env.ACCOUNT_SID,
-	authToken = process.env.AUTH_TOKEN,
-    client = require('twilio')(accountSid, authToken),
+module.exports = async function (context) {
+    const res = context.res,
     MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse()
     message = twiml.message()
-
-    console.log(res)
     
-    message.body(`This is a quokka`)
-    message.media(`https://quokkas.amyskapers.dev/img/quokka_(1).jpg`)
+    message.body(`Welcome to Quokkabot!`)
 
-    // res.writeHead(200, { 'Content-Type': 'text/xml' })
     res.set('content-type', 'text/xml')
 	res.end(message.toString())
 };
