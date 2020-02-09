56572402ce26b3a7e8bad473bac7fef681514723
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 14:59:14 +0800

    have function working locally

--- quokka_or_not/index.js
+++ quokka_or_not/index.js
@@ -1,19 +1,22 @@
-module.exports = async function (context, req, res) {
+require('dotenv').config()
+
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const bodyParser = require('body-parser'),
+    const res = context.res, bodyParser = require('body-parser'),
 	accountSid = process.env.ACCOUNT_SID,
 	authToken = process.env.AUTH_TOKEN,
     client = require('twilio')(accountSid, authToken),
-    MessagingResponse = client.twiml.MessagingResponse,
+    MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse()
     message = twiml.message()
+
+    console.log(res)
     
     message.body(`This is a quokka`)
     message.media(`https://quokkas.amyskapers.dev/img/quokka_(1).jpg`)
 
-    res.send('Welcome to quokkabot')
-
-    res.writeHead(200, { 'Content-Type': 'text/xml' })
+    // res.writeHead(200, { 'Content-Type': 'text/xml' })
+    res.set('content-type', 'text/xml')
 	res.end(message.toString())
 };
