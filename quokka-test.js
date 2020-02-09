415658f48569a04668caac8904f2229e97b6d94c
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 20:41:59 +0800

    fixed env variables

--- quokka-test.js
+++ quokka-test.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const fs = require('fs'),
 PredictionApi = require('@azure/cognitiveservices-customvision-prediction'),
 key = process.env.API_KEY,
-endpoint = process.env.API_KEY,
-publishIterationName = process.env.publishIterationName,
+endpoint = process.env.ENDPOINT,
+publishIterationName = process.env.ITERATION,
 predictor = new PredictionApi.PredictionAPIClient(key, endpoint)
 
 const customVision = async (image) => {
