a9fc8e838f2d9afae1f6762cb942b9786d2e05d1
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 18:22:32 +0800

    moving functions back in

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 
-const qs = require('querystring'),
-MessagingResponse = require('twilio').twiml.MessagingResponse
-
 module.exports = async function (context) {
     const res = context.res,
+    qs = require('querystring'),
+    MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse(),
     message = twiml.message(),
     body = qs.parse(context.req.body)
