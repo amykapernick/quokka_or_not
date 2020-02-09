55b003030c727f0fde11cdbac0b05c19217487b7
Amy Kapernick <amy@aimhigherweb.design>
2020-02-09 18:07:07 +0800

    stripping media url out of body

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -2,16 +2,17 @@ require('dotenv').config()
 
 module.exports = async function (context) {
     const res = context.res,
+    bodyParser = require('body-parser'),
     MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse(),
-    message = twiml.message()
+    message = twiml.message(),
+    image = context.req.body
 
-    // context.log(context.req.body)
-    // context.log(context.req.body.Body)
+    context.log(image.match(/MediaUrl0=((\w|\d|\%|-|\.)+)&/))
     
     message.body(
         `Welcome to Quokkabot!\n
-        ${context.req.body}`
+        ${image.match(/MediaUrl0=((\w|\d|\%|-|\.)+)&/)}`
     )
 
     res.set('content-type', 'text/xml')
