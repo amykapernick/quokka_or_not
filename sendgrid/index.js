4558e8de336d37f931c285838644ee07d6002119
Amy Kapernick <amy@aimhigherweb.design>
2020-02-11 12:17:19 +0800

    reordering functions

--- sendgrid/index.js
+++ sendgrid/index.js
@@ -8,12 +8,15 @@ module.exports = async function (context) {
     multipart = require('../parse-multipart/multipart.js'),
     bodyBuffer = Buffer.from(req.body),
     boundary = multipart.getBoundary(req.headers['content-type']),
-    body = multipart.Parse(bodyBuffer, boundary),
-    emailBuffer = Buffer.from(body[1].data),
-    email = multipart.Parse(emailBuffer, boundary)
+    body = multipart.Parse(bodyBuffer, boundary)
 
     context.log('body')
     context.log(body)
+
+    const emailBuffer = Buffer.from(body[1].data),
+    email = multipart.Parse(emailBuffer, boundary)
+
+    
     context.log('email')
     context.log(email)
     
