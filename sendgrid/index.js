7ca8218f5c1cf305b2639b740c89e3ef8b35a61f
Amy Kapernick <amy@aimhigherweb.design>
2020-02-11 11:54:07 +0800

    testing to make sure code is running

--- sendgrid/index.js
+++ sendgrid/index.js
@@ -3,42 +3,15 @@ require('dotenv').config()
 
 module.exports = async function (context) {
     context.log('sendgrid function')
+
     const req = context.req,
     multipart = require('../parse-multipart/multipart.js'),
     bodyBuffer = Buffer.from(req.body),
-    // fs = require('file-system')
-    // qs = require('querystring'),
-    // MessagingResponse = require('twilio').twiml.MessagingResponse,
-    // twiml = new MessagingResponse(),
-    // message = twiml.message(),
-    // body = qs.parse(context.req.body)
-    // boundary = ,
     boundary = multipart.getBoundary(req.headers['content-type']),
     body = multipart.Parse(bodyBuffer, boundary)
-    // body = multipart.Parse(body)
-    // text = body.Body,
-    // image = body.NumMedia && body.MediaUrl0
 
-    // if(image) {
-    //     const results = await customVision(image),
-    //     reply = whatsappReply(results)
-        
-    //     message.body(reply.message)
-
-    //     if(reply.photo) {
-    //         message.media(`https://quokkas.amyskapers.dev/img/quokka_(${reply.photo}).jpg`)
-    //     }
-    // }
-    // else {
-    //     const results = quokkaBot.quokkaBot(text)
-
-    //     message.body(results.body)
-    //     message.media(results.media)
-    // }
-
+    context.log('body')
     context.log(body)
 
-    // res.set('content-type', 'text/xml')
-	// res.end(message.toString())
 };
 
