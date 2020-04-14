8af99c15f1bfae0c9c5496bbef5bdf4096f85418
Amy Kapernick <amy@aimhigherweb.design>
2020-04-14 11:35:04 +0800

    step 2

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 
 module.exports = async function (context, req) {
     const res = context.res,
+    qs = require('querystring'),
     MessagingResponse = require('twilio').twiml.MessagingResponse,
     twiml = new MessagingResponse(),
-    message = twiml.message()
+    message = twiml.message(),
+    body = qs.parse(context.req.body)
 
-    message.body(`Welcome to Quokkabot! I'm on Twitch!`)
+
+    message.body(`Welcome to Quokkabot! I'm on Twitch!
+        ${body.MediaUrl0}
+    `)
 
     res.set('content-type', 'text/xml')
     res.end(message.toString())
